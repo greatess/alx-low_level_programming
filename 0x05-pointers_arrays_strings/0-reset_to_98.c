cdea57498ef7ec4d46ff755d7f0b9574f2f6c2df
greatess <greatess>
2022-09-20 00:03:00 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointee to an int as parameter and
+ * updates the value it point to 98
+ *
+ * @n: This is the entry
+ *
+ */
+
+void reaet_to_98(int *n)
+{
+		*n = 98;
+}
